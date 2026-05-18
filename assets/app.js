e2db875cc04c24512a2222b457cd1e4798cbf15a
Dominik Letica <info@dominikletica.de>
2026-05-18 21:31:50 +0200

    Add base assets

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,12 @@ import './stimulus_bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import alpine from 'alpinejs';
+window.Alpine = alpine;
+alpine.start();
+import apexcharts from 'apexcharts';
+window.ApexCharts = apexcharts;
+import codemirror from 'codemirror';
+window.CodeMirror = codemirror;
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
