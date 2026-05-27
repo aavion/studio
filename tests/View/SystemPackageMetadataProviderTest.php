c235e460cb72abb0823d2862fb971b454882b82e
Dominik Letica <info@dominikletica.de>
2026-05-27 21:17:09 +0200

    Align system package metadata test

--- tests/View/SystemPackageMetadataProviderTest.php
+++ tests/View/SystemPackageMetadataProviderTest.php
@@ -16,13 +16,13 @@ final class SystemPackageMetadataProviderTest extends TestCase
         self::assertSame('system', $metadata['identifier']);
         self::assertSame('aavion Studio', $metadata['name']);
         self::assertSame('Dominik Letica', $metadata['author']);
-        self::assertSame('Symfony 8 CMS (under active development)', $metadata['description']);
+        self::assertSame('Symfony 8 based content-management system for structured project websites.', $metadata['description']);
         self::assertTrue($metadata['immutable']);
         self::assertTrue($metadata['virtual']);
         self::assertSame(['frontend-theme', 'backend-theme', 'system-template'], $metadata['scopes']);
         self::assertSame('0.1.0', $metadata['version']);
         self::assertSame('MIT', $metadata['license']);
-        self::assertSame('https://github.com/aavion/studio', $metadata['homepage']);
+        self::assertSame('https://www.aavion.media', $metadata['homepage']);
         self::assertSame('https://github.com/aavion/studio', $metadata['source']);
         self::assertSame('dev-latest', $metadata['channel']);
         self::assertArrayHasKey('APP_SOURCE', $metadata['manifest']);
