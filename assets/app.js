28736fc196f95130ad13638efbf057f58d1a3e7c
Dominik Letica <info@dominikletica.de>
2026-06-13 12:37:08 +0200

    Add Symfony UX baseline packages

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,5 @@
+import { registerVueControllerComponents } from '@symfony/ux-vue';
+import { registerReactControllerComponents } from '@symfony/ux-react';
 import './stimulus_bootstrap.js';
 import './styles/app.css';
 import './js/frontend/index.js';
@@ -12,3 +14,6 @@ import './js/packages/backend-theme.js';
 import alpine from 'alpinejs';
 window.Alpine = alpine;
 alpine.start();
+
+registerReactControllerComponents();
+registerVueControllerComponents();
