9d600e8baed09d3d0ad98ef084bea155c05e1c6c
Dominik Letica <info@dominikletica.de>
2026-06-13 12:36:39 +0200

    Add route render console command

---
+++ src/Debug/RouteRenderOptions.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Debug;
+
+use App\Security\UserRole;
+
+final readonly class RouteRenderOptions
+{
+    public function __construct(
+        public string $path,
+        public string $method = 'GET',
+        public ?UserRole $role = UserRole::Owner,
+        public ?string $username = null,
+        public bool $setupCompleted = true,
+        public string $host = 'localhost',
+        public bool $secure = false,
+    ) {
+    }
+}
